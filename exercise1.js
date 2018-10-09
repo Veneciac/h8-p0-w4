9ac0cbaacb4510d851a6817e170561861cf3aa8c
Zhang <zhang@ZhangMBP15.local>
2018-10-09 11:03:49 +0700

    prima

--- exercise1.js
+++ exercise1.js
@@ -1,32 +1,19 @@
 
 // Diberikan sebuah function angkaPrima(angka) yang menerima satu parameter berupa angka. Function akan me-return true jika angka tersebut adalah bilangan prima. Jika tidak, return false.
 
-// SALAH
-// function angkaPrima(angka) {
-//   //var pembagi = [2, 3, 5, 7]
-//   if (angka > 10 && angka %2 !==0 && angka%3 !== 0 && angka %5 !== 0 &&angka % 7 !== 0 ){
-//     return true
-//   }else if (angka < 10 && angka%2 !== 0){
-//     return true
-//   }else {
-//     return false
-//   }
-// }
-
-
+SALAH
 function angkaPrima(angka) {
-  var jumlahbagi = 0 // angka prima hanya bisa dibagi dua kali 
-  for (var i = 0 ; i <= angka ; i++){// ingat <= biar angka ttp kehitung
-    if( angka % i === 0){
-      jumlahbagi ++
-    }
-  }
-  if (jumlahbagi == 2){
+  //var pembagi = [2, 3, 5, 7]
+  if (angka > 10 && angka %2 !==0 && angka%3 !== 0 && angka %5 !== 0 &&angka % 7 !== 0 ){
+    return true
+  }else if (angka < 10 && angka%2 !== 0){
     return true
   }else {
     return false
   }
 }
+
+
 
 
 // TEST CASES
