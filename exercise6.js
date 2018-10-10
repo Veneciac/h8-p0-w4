c4497300cbfbf0ea1617386e253974b0486bc4e1
Zhang <zhang@Edithaaanugroho.localdomain>
2018-10-10 15:59:37 +0700

    hitung huruf

--- exercise6.js
+++ exercise6.js
@@ -3,14 +3,42 @@
 
 function hitungHuruf(kata) {
   var arr = kata.split(' ');
-  for (let i = 0; i < arr.length; i++) {
-    for (let j = 0; j < arr[i].length; j++) {
-      if () {
-        return arr[i];
-      }
+  var pembanding = 0;
+  var index;
+  for(var i = 0; i < arr.length; i++){
+    var ulang = 0;
+
+    for (j = 0; j < arr[i].length; j++){
+      
+      for (k = arr[i].length -1; k >= 0; k--){
+
+        if (arr[i][j] === arr[i][k] && j !== k){
+        ulang++
+          } 
+        }
     }
-  }
+
+    if (ulang > pembanding){
+    pembanding = ulang
+    index = i
+    }
+  } 
+  return arr[index]
 }
+// cara lain 
+// function hitungHuruf(kata) {
+//   // you can only write your code here!
+//   let arr = kata.split(' ');
+//   for (let i = 0; i < arr.length; i++) {
+//     for (let j = 0; j < arr[i].length; j++) {
+//       if (arr[i].lastIndexOf(arr[i][j]) !== arr[i].indexOf(arr[i][j])) {
+//         return arr[i];
+//       }
+//     }
+//   }
+// }
+
+// jika dalam kata terdapat huruf yang berulang ,tampilkan kata tersebut 
 // TEST CASES
 console.log(hitungHuruf('Today, is the greatest day ever')); // greatest
 console.log(hitungHuruf('I am a passionate developer')); // passionate
@@ -18,26 +46,4 @@ console.log(hitungHuruf('aku adalah anak gembala')); // adalah
 console.log(hitungHuruf('rajin pangkal kaya')); // pangkal
 console.log(hitungHuruf('mengayuh perahu di danau')); // danau
 
-// cara 2
-// var arr = kata.split(' ');
-// var pembanding = 0;
-// var index;
-// for(var i = 0; i < arr.length; i++){
-//   var ulang = 0;
 
-//    for (j = 0; j < arr[i].length; j++){
-     
-//      for (k = arr[i].length -1; k >= 0; k--){
-
-//        if (arr[i][j] === arr[i][k] && j !== k){
-//        ulang++
-//         } 
-//       }
-//    }
-
-//   if (ulang > pembanding){
-//   pembanding = ulang
-//   index = i
-//   }
-// } 
-// return arr[index]
