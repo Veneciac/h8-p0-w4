5cb30a5b0c5e2942065d9109461a14612e3ffd7c
Zhang <zhang@Edithaaanugroho.localdomain>
2018-10-12 13:35:02 +0700

    modus

--- exercise4.js
+++ exercise4.js
@@ -1,34 +1,71 @@
-
 function cariModus(arr) {
-  var frekuensiTerbanyak= 0;
-  var Modus = 0;
-  var freq = 0;
-  for(var i = 0 ; i < arr.length ; i++){
-    // console.log(arr[i])
-    for(var j = 0 ; j < arr.length ; j++){
-      // console.log(arr[i]+" " +arr[j])
+  var nampung = {}
+  for (var i = 0 ; i < arr.length ; i++){
+    nampung[arr[i]] = 1 // MASUKIN OBJECT PAKE [] BUKAN . & harus ada nilai = brp
+    for (var j = 0 ; j < arr.length; j++) {
       if (arr[i] === arr[j] && i !== j){
-      freq ++ 
-      }else if(freq > frekuensiTerbanyak){
-        frekuensiTerbanyak = freq
-        Modus=i
-        // console.log(Modus)
-
+        nampung[arr[i]] +=1
       }
+    }   
+  }
+  var keys = Object.keys(nampung)
+  var values = Object.values(nampung)
+  //console.log(keys, values) ==> cek keys sama valuesnya
+  var max = values[0]
+  var result = keys[0]
+  for (var a = 0 ; a < values.length ; a++){
+    if(max < values[a]){
+      max = values[a]
+      result = keys[a]
     }
   }
-  if(Modus === 0){
-    return -1;
+
+  if(keys.length === 1){
+    return -1
+  } else if (max === 1){
+    return -1
   }
-  var angkaberulangsama = 0;
-  for( var k = 0 ; k < arr.length ; k++){
-    angkaberulangsama += arr[k]
-    if(angkaberulangsama/arr.length === arr[k]){
-      return -1
-    }
-  }
-  return arr[Modus]
+  // for (var k =0 ; k < nampung.length ;k++){
+  //   if (Object.values(nampung[k]) < Object.values(nampung[k+1])){
+  //     result += nampung[k +1]
+  //   }else {
+  //     result += nampung[k]
+  //   }
+  // }
+  return result
+  //return nampung //<== buat cek apakah sudah masuk ke obj 
 }
+
+// function cariModus(arr) {
+//   var frekuensiTerbanyak= 0;
+//   var Modus = 0;
+//   var freq = 0;
+//   for(var i = 0 ; i < arr.length ; i++){
+//     // console.log(arr[i])
+//     for(var j = 0 ; j < arr.length ; j++){
+//       // console.log(arr[i]+" " +arr[j])
+//       if (arr[i] === arr[j] && i !== j){
+//       freq ++ 
+//       }else if(freq > frekuensiTerbanyak){
+//         frekuensiTerbanyak = freq
+//         Modus=i
+//         // console.log(Modus)
+
+//       }
+//     }
+//   }
+//   if(Modus === 0){
+//     return -1;
+//   }
+//   var angkaberulangsama = 0;
+//   for( var k = 0 ; k < arr.length ; k++){
+//     angkaberulangsama += arr[k]
+//     if(angkaberulangsama/arr.length === arr[k]){
+//       return -1
+//     }
+//   }
+//   return arr[Modus]
+// }
 
 // TEST CASES
 console.log(cariModus([10, 4, 5, 2, 4])); // 4
@@ -67,27 +104,6 @@ console.log(cariModus([7, 7, 7, 7, 7])); // -1
 //  }
 
 //BELOM BISA
-// function cariModus(arr) {
-//   var result;
-//   var nampung = {}
-//   for (var i = 0 ; i < arr.length ; i++){
-//     nampung[arr[i]] = 1 // MASUKIN OBJECT PAKE [] BUKAN . & harus ada nilai = brp
-//     for (var j = 0 ; j < arr.length; j++) {
-//       if (arr[i] === arr[j] && i !== j){
-//         nampung[arr[i]] +=1
-//       }
-//     }   
-//   }
-//   for (var k =0 ; k < nampung.length ;k++){
-//     if (Object.values(nampung[k]) < Object.values(nampung[k+1])){
-//       result += nampung[k +1]
-//     }else {
-//       result += nampung[k]
-//     }
-//   }
-//   return result
-//   //return nampung //<== buat cek apakah sudah masuk ke obj 
-// }
 
 
 //SALAH
