469371bd896615e6bbbb8d2d4636cc93db381bc5
Zhang <zhang@Edithaaanugroho.localdomain>
2018-10-12 11:46:11 +0700

    shopping time

--- exercise12.js
+++ exercise12.js
@@ -20,9 +20,49 @@
 // Buatlah function yang akan mengembalikan sebuah object dimana object tersebut berisikan informasi memberId, money, listPurchased dan changeMoney.
 
 
+
+
+
+// function shoppingTime(memberId, money) {
+//   var result = {}
+//   var maaf = 'Mohon maaf, toko X hanya berlaku untuk member saja'
+//   if (memberId === undefined || memberId === ''){
+//     return maaf
+//   }else if( money < 50000){
+//     return 'Mohon maaf, uang tidak cukup'
+//   }else {
+//     result.memberId = memberId
+//     result.money = money
+//     result.listPurchased = []
+//     // eror
+//     //while(money >0){
+//       if (money >= 1500000){
+//         result.listPurchased.push('Sepatu Stacattu')
+//         money -= 1500000
+//       }else if (money >= 500000){
+//         result.listPurchased.push('Baju Zoro')
+//         money -= 500000
+//       }else if (money >= 250000){
+//         result.listPurchased.push('Baju H&M')
+//         money -= 250000
+//       }else if (money >= 175000){
+//         result.listPurchased.push('Sweater Uniqlo')
+//         money -= 175000
+//       }else if (money >= 50000){
+//         result.listPurchased.push('Casing Hp')
+//         money -= 50000
+//       }
+//       result.changeMoney = money
+//     //}
+//     return result
+//   }
+// }
+
+// CARA LAIN!
 function shoppingTime(memberId, money) {
   var result = {}
   var maaf = 'Mohon maaf, toko X hanya berlaku untuk member saja'
+  var barang = [['Sepatu Stacattu', 1500000], ['Baju Zoro', 500000] ,['Baju H&n', 250000], ['Sweater Uniqloo', 175000], ['Casing Hp', 50000]]
   if (memberId === undefined || memberId === ''){
     return maaf
   }else if( money < 50000){
@@ -31,31 +71,21 @@ function shoppingTime(memberId, money) {
     result.memberId = memberId
     result.money = money
     result.listPurchased = []
-    while(money >0){
-      if (money >= 1500000){
-        result.listPurchased.push('Sepatu Stacattu')
-        money -= 1500000
-      }else if (money >= 500000){
-        result.listPurchased.push('Baju Zoro')
-        money -= 500000
-      }else if (money >= 250000){
-        result.listPurchased.push('Baju H&M')
-        money -= 250000
-      }else if (money >= 175000){
-        result.listPurchased.push('Sweater Uniqlo')
-        money -= 175000
-      }else if (money >= 50000){
-        result.listPurchased.push('Casing Hp')
-        money -= 50000
+    for (var i = 0 ; i < barang.length ; i++){
+      if (money >= barang[i][1]){
+        //console.log(barang[i][0])
+        result.listPurchased.push(barang[i][0])
+        money -= barang[i][1]
       }
-      result.changeMoney = money
     }
+    result.changeMoney = money
     return result
   }
 }
 
+
 // TEST CASES
-console.log(shoppingTime('1820RzKrnWn08', 2475000));
+ console.log(shoppingTime('1820RzKrnWn08', 2475000));
   //{ memberId: '1820RzKrnWn08',
   // money: 2475000,
   // listPurchased:
